0baf6c1681868c70925d42c95a387bea9ae94b9c
Shreedhar Hegde <snhkelemane@gmail.com>
2023-03-23 15:23:40 +0530

    added morgan logger and updated cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const logger = require("morgan");
 const connectDB = require("./config/db");
 const indexRoute = require("./routes/index");
 const notesRoute = require("./routes/notes.routes");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(logger("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.options("*", cors());
+app.use(cors({ origin: "*" }));
+
+// routes
 app.use("/", indexRoute);
 app.use("/notes", notesRoute);
 
